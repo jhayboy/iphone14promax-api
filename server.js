fb08ad56a54d11f948f3bd0ece7d59c546abc753
Jhayboy <mbamarajustice1@gmail.com>
2023-10-30 23:57:58 +0100

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ require('dotenv').config();
 
 const port = process.env.PORT
 const clientId = process.env.CLIENT_ID;
+const cors = require('cors');
+
+const allowedOrigins = ['http://localhost:3000', 'https://iphone14promax.vercel.app'];
+
+// Configure CORS with allowed origins
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 
 app.get("/api", (req, res, ) => {
     const clientId = process.env.CLIENT_ID;
